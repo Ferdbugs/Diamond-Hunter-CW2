38b4ef23ed91f5d0ffa7fa8f286002b940121565
Ferdous Hossain Fahim <ferdbugs@gmail.com>
2018-11-27 23:18:12 +0800

    Added ImageView and GUI for Zoom(To be Implemented)
    Generated an ImageView for the Map to be able to use viewport functions in the image view. Planning to use viewport function to zoom in on parts of the map, for ease of user to place items.

--- src/com/neet/MapViewer/MapViewController.java
+++ src/com/neet/MapViewer/MapViewController.java
@@ -1,7 +1,14 @@
 package com.neet.MapViewer;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
 
 public class MapViewController {
 
@@ -9,16 +16,27 @@ public class MapViewController {
 
     @FXML
     private Canvas MapCanvas;
+    private GraphicsContext Graphics;
+
+    @FXML
+    private ImageView Image_ViewMap;
+    private Image MapImage;
 
     public MapViewController(){
         mapModel = new MapModel();
     }
 
     public void initialize(){
+        Graphics = MapCanvas.getGraphicsContext2D();
+        mapModel.drawmap(Graphics);
+        MapImage = MapCanvas.snapshot(new SnapshotParameters(),new WritableImage(640,640));
+        Image_ViewMap.setImage(MapImage);
+        Rectangle2D viewportRect = new Rectangle2D(0, 0, 640, 640);
+        Image_ViewMap.setViewport(viewportRect);
 
-        mapModel.drawmap(MapCanvas.getGraphicsContext2D());
 
     }
+
 
 
 }
