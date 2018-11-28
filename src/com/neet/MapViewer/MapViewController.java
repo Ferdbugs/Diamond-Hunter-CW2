764b6b6d4fa3d2288d7661b4e6849d07e4863e61
Ferdous Hossain Fahim <ferdbugs@gmail.com>
2018-11-28 14:52:49 +0800

    Completed ZoomIn ZoomOut Function
    Using view-port and event handlers for buttons in the GUI, achieved implementation of Zoom functions on the MapView.

--- src/com/neet/MapViewer/MapViewController.java
+++ src/com/neet/MapViewer/MapViewController.java
@@ -1,14 +1,20 @@
 package com.neet.MapViewer;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
+
+import javax.swing.*;
 
 public class MapViewController {
 
@@ -18,9 +24,16 @@ public class MapViewController {
     private Canvas MapCanvas;
     private GraphicsContext Graphics;
 
+
     @FXML
     private ImageView Image_ViewMap;
     private Image MapImage;
+
+    @FXML
+    private RadioButton TopLeft,TopRight,BottomLeft,BottomRight;
+
+    @FXML
+    private Button ZoomIn,ZoomOut;
 
     public MapViewController(){
         mapModel = new MapModel();
@@ -34,9 +47,39 @@ public class MapViewController {
         Rectangle2D viewportRect = new Rectangle2D(0, 0, 640, 640);
         Image_ViewMap.setViewport(viewportRect);
 
-
     }
 
+
+    public void radioSelect(ActionEvent event){
+        if(TopLeft.isSelected()){
+            if(event.getSource()==ZoomIn) {
+                Rectangle2D viewportRect = new Rectangle2D(0, 0, 320, 320);
+                Image_ViewMap.setViewport(viewportRect);
+            }
+        }
+        if(TopRight.isSelected()){
+            if(event.getSource()==ZoomIn) {
+                Rectangle2D viewportRect = new Rectangle2D(320, 0, 320, 320);
+                Image_ViewMap.setViewport(viewportRect);
+            }
+        }
+        if(BottomLeft.isSelected()){
+            if(event.getSource()==ZoomIn) {
+                Rectangle2D viewportRect = new Rectangle2D(0, 320, 320, 320);
+                Image_ViewMap.setViewport(viewportRect);
+            }
+        }
+        if(BottomRight.isSelected()){
+            if(event.getSource()==ZoomIn) {
+                Rectangle2D viewportRect = new Rectangle2D(320, 320, 320, 320);
+                Image_ViewMap.setViewport(viewportRect);
+            }
+        }
+        if(event.getSource()==ZoomOut){
+            Rectangle2D viewportRect = new Rectangle2D(0, 0, 640, 640);
+            Image_ViewMap.setViewport(viewportRect);
+        }
+    }
 
 
 }
