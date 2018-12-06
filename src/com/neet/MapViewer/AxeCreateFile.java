19a889f541f7ba2a4149bbf5d39551612f49ac5c
Muhammad Alif Danial <aldmnsri@gmail.com>
2018-12-06 16:04:30 +0800

    Added reset methods for Create File classes

--- src/com/neet/MapViewer/AxeCreateFile.java
+++ src/com/neet/MapViewer/AxeCreateFile.java
@@ -19,17 +19,24 @@ public class AxeCreateFile {
             System.out.println("Error Creating File For Axe!");
         }
     }
-        public void setX(int x){this.x=x;}
-        public void setY(int y){this.y=y;}
+    public void setX(int x){this.x=x;}
+    public void setY(int y){this.y=y;}
 
 
-        public void addRecords(){
-            file.format("%d \n %d ",x, y);
-        }
-        public void closeFile() {
-            file.close();
+    public void addRecords(){
+        file.format("%d \n %d ",x, y);
+    }
 
-        }
+    public void resetRecords(){
+        x=0;
+        y=0;
+        file.format("%d \n %d ",x, y);
+    }
+
+    public void closeFile() {
+        file.close();
+
+    }
 
 
 
--- src/com/neet/MapViewer/BoatCreateFile.java
+++ src/com/neet/MapViewer/BoatCreateFile.java
@@ -17,16 +17,23 @@ public class BoatCreateFile {
             System.out.println("Error Creating File For Boat!");
         }
     }
-        public void setX(int x){this.x=x;}
-        public void setY(int y){this.y=y;}
+    public void setX(int x){this.x=x;}
+    public void setY(int y){this.y=y;}
 
-        public void addRecords(){
-            file.format("%d \n %d ",x, y);
-        }
-        public void closeFile() {
-            file.close();
+    public void addRecords(){
+        file.format("%d \n %d ",x, y);
+    }
 
-        }
+    public void resetRecords(){
+        x=0;
+        y=0;
+        file.format("%d \n %d ",x, y);
+    }
+
+    public void closeFile() {
+        file.close();
+
+    }
 
 
 
